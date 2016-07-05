ac68c99b85a7ccdcabf9bc838ca91f41ecfc9579
juniorcsharp <pikodellamirandola@gmail.com>
2016-07-05 15:57:02 +0300

    demo

--- README.md
+++
@@ -1,2 +0,0 @@
-# VLCTranslationsCounter
-This program is tocount all TCP, which are connectedto your VLC translation. It is simple, and can be done using lybraries (with other methodics), but I did like that, because it is my first app. It shows number of TCPs in system tray.
---
+++ VLCTranslationsCounter/VLCTranslationsCounter/IconMaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+
+namespace VLCTranslationsCounter
+{
+    class IconMaker
+    {
+        public Icon MakeIcon(String text)
+        {
+
+            Color color = Color.FromArgb(175, 255, 255);
+            Font font = new Font("Times New Roman", 11.0f);
+            Brush brush = new SolidBrush(color);
+
+            // Create a bitmap and draw text on it
+            Bitmap bitmap = new Bitmap(16, 16);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.DrawString(text, font, brush, -1, 0);
+
+            // Convert the bitmap with text to an Icon
+            IntPtr hIcon = bitmap.GetHicon();
+            Icon icon = Icon.FromHandle(hIcon);
+            return icon;
+        }
+    }
+}
