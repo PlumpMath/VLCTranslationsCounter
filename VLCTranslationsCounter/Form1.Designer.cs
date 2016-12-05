28d090737f17af10696d856103f1a18251eca8f8
juniorcsharp <pikodellamirandola@gmail.com>
2016-12-05 13:45:21 +0200

    demo

---
+++ VLCTranslationsCounter/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace VLCTranslationsCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.restoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "VLC Transactions Counter 1.0";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restoreToolStripMenuItem,
+            this.closeApplicationToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(168, 48);
+            // 
+            // restoreToolStripMenuItem
+            // 
+            this.restoreToolStripMenuItem.Name = "restoreToolStripMenuItem";
+            this.restoreToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.restoreToolStripMenuItem.Text = "Restore";
+            this.restoreToolStripMenuItem.Click += new System.EventHandler(this.RestoreContextMenuItem_Click);
+            // 
+            // closeApplicationToolStripMenuItem
+            // 
+            this.closeApplicationToolStripMenuItem.Name = "closeApplicationToolStripMenuItem";
+            this.closeApplicationToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.closeApplicationToolStripMenuItem.Text = "Close Application";
+            this.closeApplicationToolStripMenuItem.Click += new System.EventHandler(this.CloseApplicationContextMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 264);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem restoreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeApplicationToolStripMenuItem;
+    }
+}
+
---
+++ VLCTranslationsCounter/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace VLCTranslationsCounter
+{
+    public partial class Form1 : Form
+    {
+        //Address
+       private static string address;    //any address of your translation
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            GetInfoFromCmd();
+        }
+
+        //For read from txt and then start cmd.
+        public void GetInfoFromCmd()
+        {
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("C:\\PCIP.txt"))
+                {
+                    // Read the stream to a string, and write the string to the address.
+                    String line = sr.ReadToEnd();
+                    address = line;
+                }
+
+                try
+                {
+                    //Creating cmd process
+                    ProcessStartInfo proc = new ProcessStartInfo();
+                    proc.FileName = @"cmd.exe";
+                    proc.RedirectStandardOutput = true;
+                    proc.Arguments = @"/c netstat -a | findstr " + address;
+
+                    //Hide window
+                    proc.WindowStyle = ProcessWindowStyle.Hidden;
+                    proc.UseShellExecute = false;
+                    proc.CreateNoWindow = true;
+
+                    //Starting this process
+                    Process cmd = Process.Start(proc);
+
+                    //Putting the result of cmd command to tempResult
+                    StreamReader reader = cmd.StandardOutput;
+                    string tempResult = reader.ReadToEnd();   //puttig all in temp
+                    string[] words = tempResult.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                    int counter = 0;
+                    //string tcp = "Reply";   //for testing
+                    string tcp = "TCP";
+                    foreach (string temp in words)   //seardhing for TCP user who is connected
+                    {
+                        if (temp.Contains(tcp))
+                        {
+                            counter++;
+                        }
+                    }
+                    String count = counter.ToString();
+
+                    // Making notifyIcon1
+                    IconMaker maker = new IconMaker();
+                    this.notifyIcon1.Icon = maker.MakeIcon(count);
+
+                    //Closing reader
+                    reader.Close();
+
+                    //Closing cmd process
+                    cmd.WaitForExit();
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());   //if any exception acquires
+                    Application.Exit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());   //if any exception acquires
+                Application.Exit();
+                
+            }
+        }
+          
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+                Hide();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void RestoreContextMenuItem_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void CloseApplicationContextMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            notifyIcon1.Visible = false;
+        }
+    }
+
+}
